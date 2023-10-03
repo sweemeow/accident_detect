27e1b44136314bb7276e9681254655ddf77339a2
sweety agrawal <sweetyagr128@gmail.com>
2023-10-03 18:37:42 +0530

    good

--- camera.py
+++ camera.py
@@ -1,13 +1,15 @@
 import cv2
 from detection import AccidentDetectionModel
 import numpy as np
-import os
+import osg
 
 model = AccidentDetectionModel("model.json", 'model_weights.h5')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+
 def startapplication():
-    video = cv2.VideoCapture('meow.mp4') # for camera use video = cv2.VideoCapture(0)
+    # for camera use video = cv2.VideoCapture(0)
+    video = cv2.VideoCapture('meow.mp4')
     count = 0
     while True:
         ret, frame = video.read()
@@ -17,19 +19,20 @@ def startapplication():
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         except Exception as e:
             break
-            
+
         roi = cv2.resize(gray_frame, (250, 250))
 
         pred, prob = model.predict_accident(roi[np.newaxis, :, :])
         if(pred == "Accident"):
             prob = (round(prob[0][0]*100, 2))
-            
+
             # to beep when alert:
             if(prob > 90):
                 print("accident")
-              
+
             cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
-            cv2.putText(frame, pred+" "+str(prob), (20, 30), font, 1, (255, 255, 0), 2)
+            cv2.putText(frame, pred+" "+str(prob),
+                        (20, 30), font, 1, (255, 255, 0), 2)
 
         if cv2.waitKey(33) & 0xFF == ord('q'):
             return
