3e5b8f9b72fd07c06fb1352094955f64e69ddfd4
sweemeow <114148751+sweemeow@users.noreply.github.com>
2023-10-03 15:54:18 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,40 @@
+import cv2
+from detection import AccidentDetectionModel
+import numpy as np
+import os
+
+model = AccidentDetectionModel("model.json", 'model_weights.h5')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+def startapplication():
+    video = cv2.VideoCapture('meow.mp4') # for camera use video = cv2.VideoCapture(0)
+    count = 0
+    while True:
+        ret, frame = video.read()
+        try:
+            count = count+1
+            print(f"processing frame number {count}")
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        except Exception as e:
+            break
+            
+        roi = cv2.resize(gray_frame, (250, 250))
+
+        pred, prob = model.predict_accident(roi[np.newaxis, :, :])
+        if(pred == "Accident"):
+            prob = (round(prob[0][0]*100, 2))
+            
+            # to beep when alert:
+            if(prob > 90):
+                print("accident")
+              
+            cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
+            cv2.putText(frame, pred+" "+str(prob), (20, 30), font, 1, (255, 255, 0), 2)
+
+        if cv2.waitKey(33) & 0xFF == ord('q'):
+            return
+        cv2.imshow('Video', frame)
+
+
+if __name__ == '__main__':
+    startapplication()
